7494fc3e48f1886f2cf0d649c595c524e310e25c
Caskey, Damon V <dc@caskeys.com>
2018-07-19 17:00:46 -0400

    Add Y and X screen loops.

--- lucias-run/lucias-run/lucias-run-main.cpp
+++ lucias-run/lucias-run/lucias-run-main.cpp
@@ -24,6 +24,27 @@ protected:
 	// Called by olcConsoleGameEngine
 	virtual bool OnUserUpdate(float fElapsedTime)
 	{
+		int y;				// Y index in loop.
+		int x;				// X index in loop.
+		int screen_y_half;	// Half of screen height.
+		int screen_x;
+
+		// Get half the screen height.
+		screen_y_half = ScreenHeight() / 2;
+
+		// Get screen width.
+		screen_x = ScreenWidth();
+
+		// Interate each line on Y axis.
+		for (y = 0; y < screen_y_half; y++)
+		{
+			// Iterate each line on X axis.
+			for (x = 0; x < screen_x; x++)
+			{
+
+			}
+		}
+
 		return true;
 	}
 };
